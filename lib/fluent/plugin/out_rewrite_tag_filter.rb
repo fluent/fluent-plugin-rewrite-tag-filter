5f456646cd0958ba6f5e800b6e99e75331688a79
y-ken <y.ken.studio@gmail.com>
2012-09-15 22:51:33 +0900

    add capitalize_regex_backreference option

--- lib/fluent/plugin/out_rewrite_tag_filter.rb
+++ lib/fluent/plugin/out_rewrite_tag_filter.rb
@@ -7,6 +7,7 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
   (2..PATTERN_MAX_NUM).each do |i|
     config_param ('rewriterule' + i.to_s).to_sym, :string, :default => nil # NAME REGEXP
   end
+  config_param :capitalize_regex_backreference, :bool, :default => false
 
   def configure(conf)
     super
@@ -16,23 +17,24 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
 
     invalids = conf.keys.select{|k| k =~ /^rewriterule(\d+)$/}.select{|arg| arg =~ /^rewriterule(\d+)/ and not (1..PATTERN_MAX_NUM).include?($1.to_i)}
     if invalids.size > 0
-      $log.warn "invalid number rewriterules (valid rewriterule number:1-{PATTERN_MAX_NUM}):" + invalids.join(",")
+      $log.warn "invalid number rewriterules (valid rewriterule number:1-{PATTERN_MAX_NUM}): #{invalids.join(",")}"
     end
     (1..PATTERN_MAX_NUM).each do |i|
       next unless conf["rewriterule#{i}"]
-      rewritekey,regexp,rewritetag = conf["rewriterule#{i}"].match(/([^ ]+)\s(.+)\s([^ ]+)/)[1,3].to_a
+      rewritekey,regexp,rewritetag = conf["rewriterule#{i}"].match(/([^ ]+)\s(.+)\s([^ ]+)/).captures
       unless regexp != nil && rewritetag != nil
-        raise Fluent::ConfigError, "missing values at " + "rewriterule#{i} " + conf["rewriterule#{i}"].inspect
+        raise Fluent::ConfigError, "missing values at rewriterule#{i} " + conf["rewriterule#{i}"].inspect
       end 
       @rewriterules.push([i, rewritekey, Regexp.new(regexp), rewritetag])
       rewriterule_names.push(rewritekey + regexp)
+      $log.info "register rewrite_tag_filter rule: #{@rewriterules.last}"
     end
     rewriterule_index_list = conf.keys.select{|s| s =~ /^rewriterule\d$/}.map{|v| (/^rewriterule(\d)$/.match(v))[1].to_i}
     unless rewriterule_index_list.reduce(true){|v,i| v and @rewriterules[i - 1]}
-      raise Fluent::ConfigError, "jump of rewriterule index found" + @rewriterules.inspect
+      raise Fluent::ConfigError, "jump of rewriterule index found #{@rewriterules.inspect}"
     end
     unless @rewriterules.length == rewriterule_names.uniq.length
-      raise Fluent::ConfigError, "duplicated rewriterules found" + @rewriterules.inspect
+      raise Fluent::ConfigError, "duplicated rewriterules found #{@rewriterules.inspect}"
     end
   end
 
@@ -45,6 +47,9 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
         if (regexp && regexp.match(rewritevalue))
           rewrite = true
           tag = rewritetag
+          if rewritetag.include?('$')
+            tag = rewritetag.gsub(/\$\d+/, map_regex_table($~.captures))
+          end
           break
         end
       end
@@ -52,6 +57,16 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
     end
 
     chain.next
+  end
+
+  def map_regex_table(elements)
+    hash_table = Hash.new
+    index = 1
+    elements.each do |value|
+      hash_table["$#{index}"] = @capitalize_regex_backreference ? value.capitalize : value
+      index += 1
+    end
+    return hash_table
   end
 end
 
--- test/plugin/test_out_rewrite_tag_filter.rb
+++ test/plugin/test_out_rewrite_tag_filter.rb
@@ -4,11 +4,20 @@ class RewriteTagFilterOutputTest < Test::Unit::TestCase
   def setup
     Fluent::Test.setup
   end
-  
+
   CONFIG = %[
     rewriterule1 domain ^www\.google\.com$ site.Google
     rewriterule2 domain ^news\.google\.com$ site.GoogleNews
     rewriterule3 agent Mac\sOS\sX agent.MacOSX
+    rewriterule4 agent (Googlebot|CustomBot)-([a-zA-Z]+) agent.$1-$2
+  ]
+
+  CONFIG2 = %[
+    capitalize_regex_backreference yes
+    rewriterule1 domain ^www\.google\.com$ site.Google
+    rewriterule2 domain ^(news)\.(google)\.com$ site.$2$1
+    rewriterule3 agent Mac\sOS\sX agent.MacOSX
+    rewriterule4 agent (Googlebot|CustomBot)-([a-zA-Z]+) agent.$1-$2
   ]
 
   def create_driver(conf=CONFIG,tag='test')
@@ -35,9 +44,10 @@ class RewriteTagFilterOutputTest < Test::Unit::TestCase
       d1.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
       d1.emit({'domain' => 'news.google.com', 'path' => '/', 'agent' => 'Googlebot-Mobile', 'response_time' => 900000})
       d1.emit({'domain' => 'map.google.com', 'path' => '/', 'agent' => 'Macintosh; Intel Mac OS X 10_7_4', 'response_time' => 900000})
+      d1.emit({'domain' => 'labs.google.com', 'path' => '/', 'agent' => 'Mozilla/5.0 Googlebot-FooBar/2.1', 'response_time' => 900000})
     end
     emits = d1.emits
-    assert_equal 3, emits.length
+    assert_equal 4, emits.length
     p emits[0]
     assert_equal 'site.Google', emits[0][0] # tag
     p emits[1]
@@ -45,6 +55,30 @@ class RewriteTagFilterOutputTest < Test::Unit::TestCase
     assert_equal 'news.google.com', emits[1][2]['domain']
     p emits[2]
     assert_equal 'agent.MacOSX', emits[2][0] #tag
+    p emits[3]
+    assert_equal 'agent.Googlebot-FooBar', emits[3][0] #tag
+  end
+
+  def test_emit2
+    d1 = create_driver(CONFIG2, 'input.access')
+    time = Time.parse("2012-01-02 13:14:15").to_i
+    d1.run do
+      d1.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
+      d1.emit({'domain' => 'news.google.com', 'path' => '/', 'agent' => 'Googlebot-Mobile', 'response_time' => 900000})
+      d1.emit({'domain' => 'map.google.com', 'path' => '/', 'agent' => 'Macintosh; Intel Mac OS X 10_7_4', 'response_time' => 900000})
+      d1.emit({'domain' => 'labs.google.com', 'path' => '/', 'agent' => 'Mozilla/5.0 Googlebot-FooBar/2.1', 'response_time' => 900000})
+    end
+    emits = d1.emits
+    assert_equal 4, emits.length
+    p emits[0]
+    assert_equal 'site.Google', emits[0][0] # tag
+    p emits[1]
+    assert_equal 'site.GoogleNews', emits[1][0] # tag
+    assert_equal 'news.google.com', emits[1][2]['domain']
+    p emits[2]
+    assert_equal 'agent.MacOSX', emits[2][0] #tag
+    p emits[3]
+    assert_equal 'agent.Googlebot-Foobar', emits[3][0] #tag
   end
 end
 
