d71f48f7e644c377767df2b0c286b951314fb67e
futoase <futoase@gmail.com>
2013-10-09 17:48:54 +0900

    Modified: Added config_param of use_of_first_match_tag.

--- lib/fluent/plugin/out_rewrite_tag_filter.rb
+++ lib/fluent/plugin/out_rewrite_tag_filter.rb
@@ -3,6 +3,7 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
 
   config_param :capitalize_regex_backreference, :bool, :default => false
   config_param :remove_tag_prefix, :string, :default => nil
+  config_param :use_of_first_match_tag, :string, :default => nil
   config_param :hostname_command, :string, :default => 'hostname'
 
   MATCH_OPERATOR_EXCLUDE = '!'
@@ -34,6 +35,10 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
 
     unless @remove_tag_prefix.nil?
       @remove_tag_prefix = /^#{Regexp.escape(@remove_tag_prefix)}\.?/
+    end
+
+    unless @use_of_first_match_tag.nil?
+      @use_of_first_match_tag = Regexp.new(@use_of_first_match_tag)
     end
   end
 
@@ -99,6 +104,7 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
 
   def get_placeholder(tag)
     tag = tag.sub(@remove_tag_prefix, '') if @remove_tag_prefix
+    tag = tag.match(@use_of_first_match_tag)[1] if @use_of_first_match_tag
     return {
       '__HOSTNAME__' => @hostname,
       '${hostname}' => @hostname,
