2f41e5c39918104a23ab295ada026a58b71bf606
Kenji Okimoto <okimoto@clear-code.com>
2017-10-13 16:06:11 +0900

    Stop relying on StandardError#message

--- lib/fluent/plugin/out_rewrite_tag_filter.rb
+++ lib/fluent/plugin/out_rewrite_tag_filter.rb
@@ -102,11 +102,10 @@ class Fluent::RewriteTagFilterOutput < Fluent::Output
   end
 
   def regexp_last_match(regexp, rewritevalue)
-    begin
-      return if regexp.nil?
+    return if regexp.nil?
+    if rewritevalue.valid_encoding?
       regexp.match(rewritevalue)
-    rescue ArgumentError => e
-      raise e unless e.message.index('invalid byte sequence in') == 0
+    else
       regexp.match(rewritevalue.scrub('?'))
     end
   end
