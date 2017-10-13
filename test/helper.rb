e48d60a0529cf9fe1b54214ca1d90b47d005fe2f
Kenji Okimoto <okimoto@clear-code.com>
2017-10-13 15:56:26 +0900

    Cleanup test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,29 +1,9 @@
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+require 'bundler/setup'
 require 'test/unit'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
+$LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
 require 'fluent/test/driver/output'
-unless ENV.has_key?('VERBOSE')
-  nulllogger = Object.new
-  nulllogger.instance_eval {|obj|
-    def method_missing(method, *args)
-      # pass
-    end
-  }
-  $log = nulllogger
-end
 
 require 'fluent/plugin/out_rewrite_tag_filter'
-
-class Test::Unit::TestCase
-end
