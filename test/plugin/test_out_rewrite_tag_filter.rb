596d4345a42bfbd0b33d6284e630e6ea737935f2
okimoto@clear-code.com <okkez000@gmail.com>
2017-10-17 15:54:36 +0900

    Add rule section

--- test/plugin/test_out_rewrite_tag_filter.rb
+++ test/plugin/test_out_rewrite_tag_filter.rb
@@ -230,4 +230,212 @@ class RewriteTagFilterOutputTest < Test::Unit::TestCase
       assert_equal invalid_ascii, emits[0][2]['client_name']
     end
   end
+
+  sub_test_case "section style" do
+    test "simple" do
+      config = %[
+        <rule>
+          key domain
+          pattern ^www\.google\.com$
+          tag site.Google
+        </rule>
+        <rule>
+          key domain
+          pattern ^news\.google\.com$
+          tag site.GoogleNews
+        </rule>
+        <rule>
+          key agent
+          pattern .* Mac OS X .*
+          tag agent.MacOSX
+        </rule>
+        <rule>
+          key agent
+          pattern (Googlebot|CustomBot)-([a-zA-Z]+)
+          tag agent.$1-$2
+        </rule>
+        <rule>
+          key domain
+          pattern ^(tagtest)\.google\.com$
+          tag site.${tag}.$1
+        </rule>
+      ]
+      d = create_driver(config, 'input.access')
+      d.run do
+        d.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
+        d.emit({'domain' => 'news.google.com', 'path' => '/', 'agent' => 'Googlebot-Mobile', 'response_time' => 900000})
+        d.emit({'domain' => 'map.google.com', 'path' => '/', 'agent' => 'Macintosh; Intel Mac OS X 10_7_4', 'response_time' => 900000})
+        d.emit({'domain' => 'labs.google.com', 'path' => '/', 'agent' => 'Mozilla/5.0 Googlebot-FooBar/2.1', 'response_time' => 900000})
+        d.emit({'domain' => 'tagtest.google.com', 'path' => '/', 'agent' => 'Googlebot', 'response_time' => 900000})
+        d.emit({'domain' => 'noop.example.com'}) # to be ignored
+      end
+      emits = d.emits
+      assert_equal 5, emits.length
+      assert_equal 'site.Google', emits[0][0] # tag
+      assert_equal 'site.GoogleNews', emits[1][0] # tag
+      assert_equal 'news.google.com', emits[1][2]['domain']
+      assert_equal 'agent.MacOSX', emits[2][0] #tag
+      assert_equal 'agent.Googlebot-FooBar', emits[3][0] #tag
+      assert_equal 'site.input.access.tagtest', emits[4][0] #tag
+    end
+    test "remove_tag_prefix" do
+      config = %[
+        remove_tag_prefix input
+        <rule>
+          key domain
+          pattern ^www\.google\.com$
+          tag ${tag}
+        </rule>
+      ]
+      d = create_driver(config, 'input.access')
+      d.run do
+        d.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
+      end
+      emits = d.emits
+      assert_equal 1, emits.length
+      assert_equal 'access', emits[0][0] # tag
+    end
+
+    test "remove_tag_prefix with dot" do
+      config = %[
+        remove_tag_prefix input.
+        <rule>
+          key domain
+          pattern ^www\.google\.com$
+          tag ${tag}
+        </rule>
+      ]
+      d = create_driver(config, 'input.access')
+      d.run do
+        d.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
+      end
+      emits = d.emits
+      assert_equal 1, emits.length
+      assert_equal 'access', emits[0][0] # tag
+    end
+
+    test "short hostname" do
+      config = %[
+        remove_tag_prefix input
+        hostname_command hostname -s
+        <rule>
+          key domain
+          pattern ^www\.google\.com$
+          tag ${hostname}
+        </rule>
+      ]
+      d = create_driver(config, 'input.access')
+      d.run do
+        d.emit({'domain' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000})
+      end
+      emits = d.emits
+      assert_equal 1, emits.length
+      assert_equal `hostname -s`.chomp, emits[0][0] # tag
+    end
+
+    test "non matching" do
+      config = %[
+        <rule>
+          key domain
+          pattern ^www\..+$
+          tag not_start_with_www
+          invert true
+        </rule>
+        <rule>
+          key domain
+          pattern ^www\..+$
+          tag start_with_www
+        </rule>
+      ]
+      d = create_driver(config, 'input.access')
+      d.run do
+        d.emit({'domain' => 'www.google.com'})
+        d.emit({'path' => '/'})
+        d.emit({'domain' => 'maps.google.com'})
+      end
+      emits = d.emits
+      assert_equal 3, emits.length
+      assert_equal 'start_with_www', emits[0][0] # tag
+      assert_equal 'not_start_with_www', emits[1][0] # tag
+      assert_equal 'not_start_with_www', emits[2][0] # tag
+    end
+
+    test "split by tag" do
+      config = %[
+        <rule>
+          key user_name
+          pattern ^Lynn Minmay$
+          tag vip.${tag_parts[1]}.remember_love
+        </rule>
+        <rule>
+          key user_name
+          pattern ^Harlock$
+          tag ${tag_parts[2]}.${tag_parts[0]}.${tag_parts[1]}
+        </rule>
+        <rule>
+          key  world
+          pattern ^(alice|chaos)$
+          tag application.${tag_parts[0]}.$1_server
+        </rule>
+        <rule>
+          key world
+          pattern ^[a-z]+$
+          tag application.${tag_parts[1]}.future_server
+        </rule>
+      ]
+      d = create_driver(config, 'game.production.api')
+      d.run do
+        d.emit({'user_id' => '10000', 'world' => 'chaos', 'user_name' => 'gamagoori'})
+        d.emit({'user_id' => '10001', 'world' => 'chaos', 'user_name' => 'sanageyama'})
+        d.emit({'user_id' => '10002', 'world' => 'nehan', 'user_name' => 'inumuta'})
+        d.emit({'user_id' => '77777', 'world' => 'space', 'user_name' => 'Lynn Minmay'})
+        d.emit({'user_id' => '99999', 'world' => 'space', 'user_name' => 'Harlock'})
+      end
+      emits = d.emits
+      assert_equal 5, emits.length
+      assert_equal 'application.game.chaos_server', emits[0][0]
+      assert_equal 'application.game.chaos_server', emits[1][0]
+      assert_equal 'application.production.future_server', emits[2][0]
+      assert_equal 'vip.production.remember_love', emits[3][0]
+      assert_equal 'api.game.production', emits[4][0]
+    end
+
+    test "invalid_byte (UTF-8)" do
+      config = %[
+        <rule>
+          key client_name
+          pattern (.+)
+          tag app.$1
+        </rule>
+      ]
+      invalid_utf8 = "\xff".force_encoding('UTF-8')
+      d = create_driver(config, 'input.activity')
+      d.run do
+        d.emit({'client_name' => invalid_utf8})
+      end
+      emits = d.emits
+      assert_equal 1, emits.length
+      assert_equal "app.?", emits[0][0]
+      assert_equal invalid_utf8, emits[0][2]['client_name']
+    end
+
+    test "invalid byte (US-ASCII)" do
+      config = %[
+        <rule>
+          key client_name
+          pattern (.+)
+          tag app.$1
+        </rule>
+      ]
+      invalid_ascii = "\xff".force_encoding('US-ASCII')
+      d = create_driver(config, 'input.activity')
+      d.run do
+        d.emit({'client_name' => invalid_ascii})
+      end
+      emits = d.emits
+      assert_equal 1, emits.length
+      assert_equal "app.?", emits[0][0]
+      assert_equal invalid_ascii, emits[0][2]['client_name']
+    end
+  end
 end
